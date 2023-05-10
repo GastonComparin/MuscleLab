5177cfd79600ea2712e801138bd3ff411e8383c3
GastonComparin <gastoncomparin92@gmail.com>
2023-05-10 13:01:45 -0300

    only backend

--- client/src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const PORT = 3001;
-export const URL = 'http://localhost:';
-export const fulfilled = 'fulfilled';
-export const pending = 'loading';
-export const rejected = 'rejected';
-
-
-
