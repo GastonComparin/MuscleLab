856a786959ed16b454b6f3cfe4ae50430f77519c
GastonComparin <gastoncomparin92@gmail.com>
2023-05-10 19:39:05 -0300

    user routes

--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -1,21 +1,15 @@
 const { Router } = require("express");
 const server = Router();
-
+//? HANDLERS
 const createNewUser = require("../Handlers/Users/createUserHandler");
 const deleteMyUser = require("../Handlers/Users/deleteUserHandler");
 const getAllUsers = require("../Handlers/Users/getAllUsersHandler");
 
-server.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
 server.get("/:id", async (req, res) => {
   res.status(200);
 });
-
 server.get("/", getAllUsers);
 server.post("/create", createNewUser);
-
 server.delete("/delete/:id", deleteMyUser);
 
 module.exports = server;
